66884aff14b3d2e0f631e2c4520e14ef583b4344
LunaticFish <hutongyu@vip.qq.com>
2021-01-22 10:11:34 +0800

    magic call

--- src/MatomoMagicCall.php
+++ src/MatomoMagicCall.php
@@ -3,7 +3,53 @@
 
 namespace Matomo\ReportingApi;
 
-
+/**
+ * Class MatomoMagicCall
+ * @package Matomo\ReportingApi
+ * @property-read  \Matomo\ReportingApi\API\Actions $Actions
+ * @property-read  \Matomo\ReportingApi\API\Annotations $Annotations
+ * @property-read  \Matomo\ReportingApi\API\API $API
+ * @property-read  \Matomo\ReportingApi\API\Contents $Contents
+ * @property-read  \Matomo\ReportingApi\API\CoreAdminHome $CoreAdminHome
+ * @property-read  \Matomo\ReportingApi\API\CorePluginsAdmin $CorePluginsAdmin
+ * @property-read  \Matomo\ReportingApi\API\CustomDimensions $CustomDimensions
+ * @property-read  \Matomo\ReportingApi\API\CustomJsTracker $CustomJsTracker
+ * @property-read  \Matomo\ReportingApi\API\Dashboard $Dashboard
+ * @property-read  \Matomo\ReportingApi\API\DevicePlugins $DevicePlugins
+ * @property-read  \Matomo\ReportingApi\API\DevicesDetection $DevicesDetection
+ * @property-read  \Matomo\ReportingApi\API\Events $Events
+ * @property-read  \Matomo\ReportingApi\API\Feedback $Feedback
+ * @property-read  \Matomo\ReportingApi\API\Goals $Goals
+ * @property-read  \Matomo\ReportingApi\API\ImageGraph $ImageGraph
+ * @property-read  \Matomo\ReportingApi\API\Insights $Insights
+ * @property-read  \Matomo\ReportingApi\API\LanguagesManager $LanguagesManager
+ * @property-read  \Matomo\ReportingApi\API\Live $Live
+ * @property-read  \Matomo\ReportingApi\API\Login $Login
+ * @property-read  \Matomo\ReportingApi\API\Marketplace $Marketplace
+ * @property-read  \Matomo\ReportingApi\API\MobileMessaging $MobileMessaging
+ * @property-read  \Matomo\ReportingApi\API\MultiSites $MultiSites
+ * @property-read  \Matomo\ReportingApi\API\Overlay $Overlay
+ * @property-read  \Matomo\ReportingApi\API\PagePerformance $PagePerformance
+ * @property-read  \Matomo\ReportingApi\API\PrivacyManager $PrivacyManager
+ * @property-read  \Matomo\ReportingApi\API\Referrers $Referrers
+ * @property-read  \Matomo\ReportingApi\API\Resolution $Resolution
+ * @property-read  \Matomo\ReportingApi\API\ScheduledReports $ScheduledReports
+ * @property-read  \Matomo\ReportingApi\API\SegmentEditor $SegmentEditor
+ * @property-read  \Matomo\ReportingApi\API\SEO $SEO
+ * @property-read  \Matomo\ReportingApi\API\SitesManager $SitesManager
+ * @property-read  \Matomo\ReportingApi\API\Tour $Tour
+ * @property-read  \Matomo\ReportingApi\API\Transitions $Transitions
+ * @property-read  \Matomo\ReportingApi\API\TwoFactorAuth $TwoFactorAuth
+ * @property-read  \Matomo\ReportingApi\API\UserCountry $UserCountry
+ * @property-read  \Matomo\ReportingApi\API\UserId $UserId
+ * @property-read  \Matomo\ReportingApi\API\UserLanguage $UserLanguage
+ * @property-read  \Matomo\ReportingApi\API\UsersManager $UsersManager
+ * @property-read  \Matomo\ReportingApi\API\VisitFrequency $VisitFrequency
+ * @property-read  \Matomo\ReportingApi\API\VisitorInterest $VisitorInterest
+ * @property-read  \Matomo\ReportingApi\API\VisitsSummary $VisitsSummary
+ * @property-read  \Matomo\ReportingApi\API\VisitTime $VisitTime
+ * @author LunaticFish<hutongyu@vip.qq.com>
+ */
 class MatomoMagicCall extends QueryFactory
 {
     private $module_name;
@@ -24,11 +70,12 @@ class MatomoMagicCall extends QueryFactory
     }
 
     public function __call($name,$arguments){
-        $query = $this->getQuery(
-            $this->getModuleName().
-            $this->getMethodName()
-        );
-        var_dump($arguments);
+        $this->setMethodName($name);
+
+        $methodName = "{$this->getModuleName()}.{$this->getMethodName()}";
+        $query = $this->getQuery($methodName);
+
+        return $query;
     }
 
     /**
